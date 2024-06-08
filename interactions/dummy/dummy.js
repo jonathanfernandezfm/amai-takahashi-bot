c48dc3d65e24d9b3d3f6ed1b03411efd887906aa
jonathanfernandezfm <jonathanfernandezfm@gmail.com>
2024-06-08 21:57:04 +0300

    Dummy interaction

---
+++ interactions/dummy/dummy.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'dummy',
+	description: 'Color button interaction',
+
+	/**
+	 *
+	 * @param {CommandInteraction} interaction
+	 * @returns
+	 */
+	execute: async () => {
+		console.log('Dummy interaction executed!');
+	},
+};
