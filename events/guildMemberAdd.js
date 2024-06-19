de202efae5957c693c89a057c8e566efe31f958f
jonathanfernandezfm <jonathanfernandezfm@gmail.com>
2024-06-20 00:24:39 +0300

    Role check on member add and fix startup

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 const { Events, EmbedBuilder, CommandInteraction } = require('discord.js');
-const { ROLE_FREE_MEMBER, WELCOME_CHANNEL } = require('../utils/config');
+const { ROLE_FREE_MEMBER, WELCOME_CHANNEL, TIER_ROLES } = require('../utils/config');
 const logger = require('../utils/logger');
 
 module.exports = {
@@ -13,15 +13,29 @@ module.exports = {
 	async execute(member) {
 		logger.info('Member joined');
 		console.log(member);
-		let role = await member.guild.roles.cache.get(ROLE_FREE_MEMBER);
-		if (role === undefined) {
-			role = await member.guild.roles.fetch(ROLE_FREE_MEMBER);
-		}
-		await member.roles.add(role);
+
+    let isSubscribed = false;
+    TIER_ROLES.forEach((tierRole) => {
+      const memberRoles = member.roles.cache;
+      if (memberRoles && memberRoles.has(tierRole.id)) {
+        if (!member.displayName.startsWith(`[${tierRole.icon}] `)) {
+          member.setNickname(`[${tierRole.icon}] ${member.displayName}`);
+          isSubscribed = true;
+        }
+      }
+    });
+
+    if (!isSubscribed) {
+      let role = await member.guild.roles.cache.get(ROLE_FREE_MEMBER);
+      if (role === undefined) {
+        role = await member.guild.roles.fetch(ROLE_FREE_MEMBER);
+      }
+      await member.roles.add(role);
+    }
 
 		const channel = await member.guild.channels.fetch(WELCOME_CHANNEL);
 		if (!channel) return;
-
+    
 		const embed = new EmbedBuilder()
 			.setThumbnail(member.user.displayAvatarURL())
 			.setDescription(
