ab7ff8c846140bbf09a09f55cf5b340be9ebeae3
Jonathan Fernandez Mertanen <jonathanfernandezfm@gmail.com>
2025-05-03 19:33:00 +0300

    Updated visitors message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -16,18 +16,18 @@ module.exports = {
 
 		let isSubscribed = false;
 		TIER_ROLES.forEach((tierRole) => {
-		  const memberRoles = member.roles.cache;
-		  if (memberRoles && memberRoles.has(tierRole.id)) {
-			isSubscribed = true;
-		  }
+			const memberRoles = member.roles.cache;
+			if (memberRoles && memberRoles.has(tierRole.id)) {
+				isSubscribed = true;
+			}
 		});
 
 		if (!isSubscribed) {
-		  let role = await member.guild.roles.cache.get(ROLE_FREE_MEMBER);
-		  if (role === undefined) {
-		    role = await member.guild.roles.fetch(ROLE_FREE_MEMBER);
-		  }
-		  await member.roles.add(role);
+			let role = await member.guild.roles.cache.get(ROLE_FREE_MEMBER);
+			if (role === undefined) {
+				role = await member.guild.roles.fetch(ROLE_FREE_MEMBER);
+			}
+			await member.roles.add(role);
 		}
 
 		const channel = await member.guild.channels.fetch(WELCOME_CHANNEL);
@@ -36,11 +36,11 @@ module.exports = {
 		const embed = new EmbedBuilder()
 			.setThumbnail(member.user.displayAvatarURL())
 			.setDescription(
-				`🩵 Welcome to Amai Takahashi's Community, ${member.user} (${member.user.username})!\n\n***Please wait to be verified and assigned a role, an admin will do it shortly***\n`
+				`💛 Welcome to **Bastet Figures's** Discord, ${member.user} (${member.user.username})!\n\nRemember to visit the <id:guide>\nFAQ: <#1360193709630689310>\nSay **Hi!** in <#1134920268469518451>`
 			)
-			.setColor(0x3498eb)
+			.setColor(0xebbd34)
 			.setTimestamp();
 
-		await channel.send({ content: `${member.user}`,  embeds: [embed] });
+		await channel.send({ content: `${member.user}`, embeds: [embed] });
 	},
 };
